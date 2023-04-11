755d76883b5f102527a970158f3f152ac0879483
Rafael Aldana <rafaelaldana6@Gmail.com>
2023-04-10 21:42:16 -0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Button, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 47.6062,
+    longitude: -122.3321,
+    latitudeDelta: 8.5,
+    longitudeDelta: 8.5,
+  });
+
+  const userFlot = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if(status !== 'granted'){
+      setErrorMsg('Permission to access current location was denied!');
+    }
+    let location = await Location.getCurrentPositionAsync({enableHigAccuracy: true});
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 8.5,
+      longitudeDelta: 8.5,
+    });
+    console.log(location.coords.latitude, location.coords.longitude);
+  }
+
+  useEffect(() => {
+    userFlot();
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+      mapRegion={mapRegion}
+      >
+        <Marker coordinate={mapRegion} title='Marker'/>
+        </MapView>
+        <Button title='Get my location' onPress={userFlot}/>
     </View>
   );
 }
@@ -13,8 +44,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
---
+++ Readme.md
@@ -0,0 +1,42 @@
+# LAB - Class 41
+
+## Project: js-401d51-class-41
+
+### Author: Rafael Aldana
+
+### Problem Domain  
+
+As a user I want to know where I am with percise accuracy with a plus or minus 10m error in self location.
+As a user I want to have the ability to deny to share my location.
+
+### Links and Resources
+
+- [GitHub Actions ci/cd](https://github.com/rkgallaway/server-deployment-practice-d51/actions) 
+- [GitHub-PR]()
+
+### Collaborators
+
+### Setup
+
+- npx expo install
+
+#### How to initialize/run your application (where applicable)
+
+- npm expo start --tunnel
+
+#### How to use your library (where applicable)
+
+#### Features / Routes
+
+- Feature One: 
+Gets current location of user.
+
+#### Tests
+
+- How do you run tests?
+- Any tests of note?
+- Describe any tests that you did not complete, skipped, etc
+
+#### UML
+
+![UML]()
